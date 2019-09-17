5058a049ca74f1b1dbfbb9a8e0555fddeccd0bb1
caioavelar <caioavelar17@hotmail.com>
2019-09-17 16:09:24 -0300

    2 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,17 +2,20 @@ export const products = [
   {
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    imageUrl:'' //'https://www.lifewire.com/thmb/GGQdFhKxulx9XEdR5c1OD4Gw7Y8=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/random-numbers-over-blackboard-166043947-57bb63065f9b58cdfd31d1fe.jpg'
   },
   {
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    imageUrl: ''
   },
   {
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    description: '',
+    imageUrl: ''
   }
 ];
 
